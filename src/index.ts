ba79543ea4274ab197f8474f4b227c53163d8679
Elijah Cobb <elijah@elijahcobb.com>
2019-08-06 21:58:43 -0400

    initial write

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,49 @@
  * https://elijahcobb.com
  *
  */
+
+import { ArrayList } from "@ejc-tsds/arraylist";
+
+export class Set<T> {
+
+	private list: ArrayList<T>;
+
+	public constructor(...values: T[]) {
+
+		this.list = new ArrayList<T>(...values);
+
+
+	}
+
+	public add(value: T): void {
+
+		if (this.list.contains(value)) return;
+		this.list.add(value);
+
+	}
+
+	public remove(value: T): void {
+
+		this.list.removeValue(value);
+
+	}
+
+	public contains(value: T): boolean {
+
+		return this.list.contains(value);
+
+	}
+
+	public toArray(): T[] {
+
+		return this.list.toArray();
+
+	}
+
+	public fromArray(value: T[]): void {
+
+		this.list.fromArray(value);
+
+	}
+
+}
