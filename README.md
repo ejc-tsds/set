8293339a0d43319287dce8db9afe23edcf8181ed
Elijah Cobb <elijah@elijahcobb.com>
2019-10-26 13:35:03 -0400

    v0.1.1

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # set
+Set is a simple generic data structure representing a set.
+It has an iterator built in and manages dupliates.
+
+## Methods
+```typescript
+/**
+ * Adds a value to the set if the value does not already exist.
+ * @param value A new unique value to be added to the set.
+ */
+add(value: T): void;
+
+/**
+* Remove a specific value form the set.
+* @param value The value to be removed
+*/
+remove(value: T): void;
+
+/**
+* Check if the set contains a specific value.
+* @param value The value to search for.
+* @returns {boolean} Whether the value is in the set.
+*/
+contains(value: T): boolean;
+
+/**
+* Convert the set to a native array.
+* @returns {T[]} A new Array.
+*/
+toArray(): T[];
+
+/**
+* Convert the set to an ArrayList.
+* @returns {ArrayList<T>} A new ArrayList.
+*/
+toArrayList(): ArrayList<T>;
+
+/**
+* Populate the set with a native Array. Duplicates will not be added to the set.
+* @param value An Array to populate the set from.
+*/
+fromArray(value: T[]): void;
+```
--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,49 @@
  */
 
 import { ArrayList } from "@ejc-tsds/arraylist";
+import { Iter } from "@ejc-tsds/iter";
 
-export class Set<T> {
+export class Set<T> extends Iter<T> {
 
 	private list: ArrayList<T>;
 
+	/**
+	 * Create a new instance of a Set passing in values of type T.
+	 * @param values An array of types T using the spread operator.
+	 */
 	public constructor(...values: T[]) {
 
+		super();
 		this.list = new ArrayList<T>(...values);
-
 
 	}
 
+	/**
+	 * A protected function to allow a set to be iterable. Gets a value for a specific index.
+	 * @param index The index of the value to retrieve.
+	 * @returns {T} The object as a defined value.
+	 */
+	protected iterHandler(index: number): T {
+
+		return this.list.get(index) as T;
+
+	}
+
+
+	/**
+	 * A protected function to allow a set to be iterable. Gets the size of the set.
+	 * @returns {number} The size of the set to iterate over.
+	 */
+	protected iterSize(): number {
+
+		return this.list.size();
+
+	}
+
+	/**
+	 * Adds a value to the set if the value does not already exist.
+	 * @param value A new unique value to be added to the set.
+	 */
 	public add(value: T): void {
 
 		if (this.list.contains(value)) return;
@@ -26,27 +57,57 @@ export class Set<T> {
 
 	}
 
+	/**
+	 * Remove a specific value form the set.
+	 * @param value The value to be removed
+	 */
 	public remove(value: T): void {
 
 		this.list.removeValue(value);
 
 	}
 
+	/**
+	 * Check if the set contains a specific value.
+	 * @param value The value to search for.
+	 * @returns {boolean} Whether the value is in the set.
+	 */
 	public contains(value: T): boolean {
 
 		return this.list.contains(value);
 
 	}
 
+
+	/**
+	 * Convert the set to a native array.
+	 * @returns {T[]} A new Array.
+	 */
 	public toArray(): T[] {
 
 		return this.list.toArray();
 
 	}
 
+
+	/**
+	 * Convert the set to an ArrayList.
+	 * @returns {ArrayList<T>} A new ArrayList.
+	 */
+	public toArrayList(): ArrayList<T> {
+
+		return this.list;
+
+	}
+
+	/**
+	 * Populate the set with a native Array. Duplicates will not be added to the set.
+	 * @param value An Array to populate the set from.
+	 */
 	public fromArray(value: T[]): void {
 
-		this.list.fromArray(value);
+		this.list = new ArrayList<T>();
+		for (const v of value) this.add(v);
 
 	}
 
